9f12244d2b5cfbdaf7f983e830eaf71e2feebf04
Nataliia Manuilova <nataliia_manuilova@epam.com>
2018-03-29 19:07:03 +0300

    A007 - html now parsed to json using Gson. code refactoring

--- src/main/java/ua/nure/manuilova/grabber/Grabber.java
+++ src/main/java/ua/nure/manuilova/grabber/Grabber.java
@@ -1,9 +1,6 @@
 package ua.nure.manuilova.grabber;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,38 +8,15 @@ import java.util.List;
 
 public class Grabber {
     public static void main(String[] args) throws IOException {
-        String url = "http://daily-menu.ru/dailymenu/recipes/view/12763";
-
-        Document doc = Jsoup.connect(url).get();
-        Elements recipeTitle = doc.getElementsByClass("recipe_title");
-        RecipeDto recipeDto = new RecipeDto();
-        recipeDto.setRecipeName(recipeTitle.first().ownText());
-
-        Elements recipeCalculation = doc.select(".recipe_calculation > tbody");
-
-        Elements ingredients = recipeCalculation.first().getElementsByTag("tr");
-
-        for (Element ingredient : ingredients) {
-            ProductDto productDto = new ProductDto();
-            Elements productName = ingredient.getElementsByAttribute("itemprop");
-            productDto.setProductName(productName.text());
-            Elements variables = ingredient.getElementsByClass("variable");
-            List<String> variablesList = new ArrayList<>();
-            for (Element variable : variables) {
-                variablesList.add(variable.ownText());
-            }
-            productDto.setWeight(parseNumber(variablesList.get(0)));
-            productDto.setKcal(parseNumber(variablesList.get(1)));
-            productDto.setProteins(parseNumber(variablesList.get(2)));
-            productDto.setFats(parseNumber(variablesList.get(3)));
-            productDto.setCarbohydrates(parseNumber(variablesList.get(4)));
-            recipeDto.addProduct(productDto);
+        List<RecipeDto> recipeDtos = new ArrayList<>();
+        for (int i = 119; i <= 119; ++i) {
+            String link = "http://daily-menu.ru/dailymenu/recipes/view/" + i;
+            RecipeDto dto = new DailyMenuRecipeDocumentParser().getRecipe(link);
+            recipeDtos.add(dto);
+            Gson json = new Gson();
+            System.out.println(json.toJson(dto));
         }
-
-        System.out.println(recipeDto);
-    }
-
-    private static Double parseNumber(String number) {
-        return number.contains("-") ? 0 : Double.parseDouble(number);
+        Gson json = new Gson();
+        System.out.println(json.toJson(recipeDtos));
     }
 }
--- src/main/java/ua/nure/manuilova/grabber/RecipeDto.java
+++ src/main/java/ua/nure/manuilova/grabber/RecipeDto.java
@@ -9,7 +9,30 @@ import lombok.Setter;
 
 @Data
 public class RecipeDto {
+    private String sourceLink = "";
     private String recipeName = "";
+    private String imageLink = "";
+    private Double totalWeight;
+    private Double totalKcal;
+    private Double totalProteins;
+    private Double totalFats;
+    private Double totalCarbohydrates;
+    private Double weightPerPortion;
+    private Double kcalPerPortion;
+    private Double proteinsPerPortion;
+    private Double fatsPerPortion;
+    private Double carbohydratesPerPortion;
+    private Double kcalPer100g;
+    private Double proteinsPer100g;
+    private Double fatsPer100g;
+    private Double carbohydratesPer100g;
+    private Double proteinsPercent;
+    private Double fatsPercent;
+    private Double carbohydratesPercent;
+    private String cookTime;
+    private String advancedCookTime;
+    private String preparation;
+
     @Setter(AccessLevel.PRIVATE)
     private List<ProductDto> products = new ArrayList<>();
 
